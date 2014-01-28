0767f7de1e52886719a80381d784374232fb6199
aviflombaum <avi@flombaum.com>
2014-01-28 14:27:07 -0500

    Done

---
+++ README.md
@@ -0,0 +1,16 @@
+---
+tags: basics
+language: ruby
+---
+
+# Hello World
+
+In your hello_world_spec.rb simply add a line to puts "Hello World!"
+
+*Note* When you run your test suite, you will not see the output, you will only see RSpec output. To test your actual output, go into IRB
+
+```
+puts "Hello World!"
+```
+
+And you'll get a better sense of how that works.
---
+++ spec/hello_world_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+describe "Hello World" do 
+
+  it 'puts "Hello World!"' do
+    # $stdout represents the output of your program, don't worry about how
+    # we are testing this, focus on simply coding your solution.
+    expect($stdout).to receive(:puts).with("Hello World!")
+    # Get your program to puts "Hello World!"
+    # Code your solution right here.
+  end
+
+end
